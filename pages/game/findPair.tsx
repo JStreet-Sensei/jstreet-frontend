3a9404b8a51e3cdadba3e1b5c25f732fd6cb9617
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-30 14:26:45 +0900

    feat:Add pages

---
+++ pages/game/findPair.tsx
@@ -0,0 +1,17 @@
+//User can play quick answer game.
+
+import React, { useState, useEffect } from "react";
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+const FindPair = () => {
+
+
+  return (
+    <div>
+This is FindPair.
+    </div>
+  );
+};
+
+export default FindPair;
