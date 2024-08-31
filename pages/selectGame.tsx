fc829313ec7e7a0c402b3e78730227b79e6c1cda
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-31 18:59:57 +0900

    feat:Move to selectgame page after login

--- pages/selectGame.tsx
+++ pages/selectGame.tsx
@@ -2,15 +2,32 @@
 
 import React, { useState, useEffect } from "react";
 
+const gameFileNames = ["newExpression", "practice"]
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
+
+
+
+
 const SelectGame = () => {
+  const [gameName, setGameName] = useState(gameFileNames[0])
+  const goRightGame = () => {
+    setGameName(gameFileNames[(gameFileNames.indexOf(gameName) + 1) % gameFileNames.length])
+  }
+  const goLeftGame = () => {
+    setGameName(gameFileNames[(gameFileNames.indexOf(gameName) + gameFileNames.length - 1) % gameFileNames.length])
+  }
 
 
   return (
-    <div>
-This is select game page.
-    </div>
+    <>
+      <button onClick={goLeftGame}>go right</button>
+      <div>
+        This is select game page.
+        {gameName}
+      </div>
+      <button onClick={goRightGame}>go left</button>
+    </>
   );
 };
 
