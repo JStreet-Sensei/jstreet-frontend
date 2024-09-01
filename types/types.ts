a22b1eff55ddc418549f64103b00c49fae9601a5
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-01 15:35:41 +0900

    feat:Put basic learning components with small state management

---
+++ types/types.ts
@@ -0,0 +1,17 @@
+interface learningContextType {
+    topic: string
+    phrase: phraseType
+    selectPhrase:React.Dispatch<React.SetStateAction<phraseType>>
+}
+
+interface phraseType {
+    id: number
+    japanese: string
+    english?: string
+    description?: string
+}
+
+
+export type {
+    learningContextType, phraseType
+}
