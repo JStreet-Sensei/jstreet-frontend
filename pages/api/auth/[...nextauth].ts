cbdf5fa5fc7c4dc2f01ba740e400a985db4b1bdf
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-01 12:05:23 +0900

    fix:Try to redirect selectgame page after login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,17 +4,21 @@ import GoogleProvider from "next-auth/providers/google"
 
 let googleClientId = process.env.GOOGLE_CLIENT_ID
 let googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+let frontOrigin = process.env.FRONT_BASE_URL
+let afterLoginUrl = process.env.FRONT_URL_AFTER_LOGIN
 let baseUrl = process.env.FRONT_BASE_URL
 let url = process.env.FRONT_URL_AFTER_LOGIN
 
 if (googleClientId === undefined) googleClientId = ""
 if (googleClientSecret === undefined) googleClientSecret = ""
-if (baseUrl === undefined) baseUrl = ""
-if (url === undefined) url = ""
+// if (baseUrl === undefined) baseUrl = ""
+// if (url === undefined) url = ""
+if (frontOrigin === undefined) frontOrigin = ""
+if (afterLoginUrl === undefined) afterLoginUrl = ""
 
 interface urlSets {
-    url: string
-    baseUrl: string
+    afterLoginUrl: string
+    frontOrigin: string
 }
 
 export const authOptions = {
@@ -27,15 +31,27 @@ export const authOptions = {
         // ...add more providers here
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async redirect({ url, baseUrl }: urlSets) {
-            // Allows relative callback URLs
-            if (url.startsWith("/")) return `${baseUrl}${url}`
-            // Allows callback URLs on the same origin
-            else if (new URL(url).origin === baseUrl) return url
-            return baseUrl
-        }
-    }
+    // callbacks: {
+    //     // async redirect({ url:afterLoginUrl, baseUrl:frontOrigin }: urlSets) {
+    //     async redirect({ url, baseUrl }) {
+    //         console.log(frontOrigin)
+    //         console.log(afterLoginUrl)
+    //         // Allows relative callback URLs
+    //         if (afterLoginUrl.startsWith("/")) return `${frontOrigin}${afterLoginUrl}`
+    //         // Allows callback URLs on the same origin
+    //         else if (new URL(afterLoginUrl).origin === frontOrigin) return afterLoginUrl
+    //         return frontOrigin
+    //     }
+    //     // async redirect({ afterLoginUrl, frontOrigin }:urlSets) {
+    //     //     console.log(frontOrigin)
+    //     //     console.log(afterLoginUrl)
+    //     //     // Allows relative callback URLs
+    //     //     if (afterLoginUrl.startsWith("/")) return `${frontOrigin}${afterLoginUrl}`
+    //     //     // Allows callback URLs on the same origin
+    //     //     else if (new URL(afterLoginUrl).origin === frontOrigin) return afterLoginUrl
+    //     //     return frontOrigin
+    //     // }
+    // }
 }
 
 export default NextAuth(authOptions)
