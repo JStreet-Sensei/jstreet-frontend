c8463d9e214d6cb9b5f08f5f46e59b897e120e46
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-01 12:12:57 +0900

    feat:Add basic components to learning and practice game

--- components/game/newExpression/start.tsx
+++ components/game/newExpression/start.tsx
@@ -1,6 +1,13 @@
-import Image from 'next/image'
+import Image, { ImageLoader } from 'next/image'
+import gamePic from "./../../../public/new-expression-fox.png
 
-const NewExpressionStart: React.FC = () => {
+const baseUrl = process.env.NEXTAUTH_URL
+
+const imageLoader: ImageLoader = ({ src, width, quality }) => {
+    return `${baseUrl}/${src}?w=${width}&q=${quality || 75}`
+}
+
+const LearningStart: React.FC = () => {
     return (<>
         <div className="grid grid-cols-2 gap-4">
             <div>
@@ -8,10 +15,10 @@ const NewExpressionStart: React.FC = () => {
                 <span> Learning Mode</span>
             </div>
             <div>
-                <Image src="../public/new-expression-fox" alt='Fox picture' />
+                <Image loader={imageLoader} width={300} height={300} src={gamePic} alt='Cross nife picture' />
             </div>
         </div>
     </>)
 }
 
-export default NewExpressionStart
+export default LearningStart
