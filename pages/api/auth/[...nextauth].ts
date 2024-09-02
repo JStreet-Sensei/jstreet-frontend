b9332205617b13a675a31d021476699f8c892bfb
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-02 12:18:20 +0900

    chore:Delete unnecessary code

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,13 +4,9 @@ import GoogleProvider from "next-auth/providers/google"
 
 let googleClientId = process.env.GOOGLE_CLIENT_ID
 let googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
-let baseUrl = process.env.FRONT_BASE_URL
-let url = process.env.FRONT_URL_AFTER_LOGIN
 
 if (googleClientId === undefined) googleClientId = ""
 if (googleClientSecret === undefined) googleClientSecret = ""
-if (baseUrl === undefined) baseUrl = ""
-if (url === undefined) url = ""
 
 interface urlSets {
     url: string
@@ -27,15 +23,7 @@ export const authOptions = {
         // ...add more providers here
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async redirect({ url, baseUrl }: urlSets) {
-            // Allows relative callback URLs
-            if (url.startsWith("/")) return `${baseUrl}${url}`
-            // Allows callback URLs on the same origin
-            else if (new URL(url).origin === baseUrl) return url
-            return baseUrl
-        }
-    }
+    //add calbacks here.
 }
 
 export default NextAuth(authOptions)
