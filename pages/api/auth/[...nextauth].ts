35ec8deb5163f6491c356b75fe886a5d4eb0a6b6
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-02 17:03:40 +0900

    Point

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,23 +1,55 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth, { Account, Profile } from "next-auth";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next";
+import { AuthenticatedUser } from "../../../globals/types/next";
+import axios from "axios";
 
+let googleClientId = process.env.GOOGLE_CLIENT_ID;
+let googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
-let googleClientId = process.env.GOOGLE_CLIENT_ID
-let googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+if (googleClientId === undefined) googleClientId = "";
+if (googleClientSecret === undefined) googleClientSecret = "";
 
-if (googleClientId === undefined) googleClientId = ""
-if (googleClientSecret === undefined) googleClientSecret = ""
+export const settings: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: googleClientId,
+      clientSecret: googleClientSecret,
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log("I am inside calling the callback");
+      if (account?.provider === "google") {
+        const { accessToken, idToken } = account;
 
-export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        GoogleProvider({
-            clientId: googleClientId,
-            clientSecret: googleClientSecret,
-        }),
-        // ...add more providers here
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-}
+        // POST request to DRF
+        try {
+          console.log("I am calling the callback");
+          const response = await axios.post(
+            "http://localhost:8000/api/google/",
+            {
+              access_token: accessToken,
+              id_token: idToken,
+            },
+          );
 
-export default NextAuth(authOptions)
+          const { access_token } = response.data;
+          return true;
+        } catch (error) {
+          console.log("Error: ", error);
+          return false;
+        }
+      }
+      return false;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, settings);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const HomePage = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    // Fetch data from the backend using a GET request
-    fetch(BACKEND_URL + "/test/")
-      .then((res) => res.json())
-      .then((data) => setData(data.data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []); // Empty dependency array ensures this runs only once on component mount
-
   return (
     <div>
       <h1>Dockerised Full-Stack Template</h1>
-      <h3>With Django, React, Postgres, and Docker</h3>
-      <p>{data ? data : "Loading data..."}</p>{" "}
-      {/* Display data or loading message */}
     </div>
   );
 };
