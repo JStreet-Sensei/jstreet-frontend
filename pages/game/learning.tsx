97f889d32f6f662535441e09359f100f31a47613
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-02 19:05:29 +0900

    chore:Modify typo

--- pages/game/learning.tsx
+++ pages/game/learning.tsx
@@ -49,7 +49,7 @@ const Learning = () => {
             })}
           </div>
           <div>
-            Each pharese list
+            Each phrase list
             <PhraseList></PhraseList>
           </div>
         </div>
