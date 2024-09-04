1994030c39c78b05f640297967cc22e2dc0b438b
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-04 14:17:53 +0900

    Add the feature of guess card locally

--- components/game/findPairGame/Card.tsx
+++ components/game/findPairGame/Card.tsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
+
 type CardProps = {
   frontText: string;
   backText: string;
+  selectCard: (index: number, lastState: boolean) => boolean;
+  index: number;
+  guessed: number;
+  selected: boolean;
 };
 
-export const Card: React.FC<CardProps> = ({ frontText, backText }) => {
+export const Card: React.FC<CardProps> = ({
+  frontText,
+  backText,
+  selectCard,
+  index,
+  selected,
+  guessed,
+}) => {
+  // Flip true means that show the back of the cards
+  const [useFlip, setFlip] = useState(false);
+
+  // Flip the card on states of gueesed and selected
+  useEffect(() => {
+    if (selected) setFlip(true);
+    else if (guessed !== 0) setFlip(true);
+    else setFlip(false);
+  }, [selected, guessed]);
+
+  const handleFlip = () => {
+    if (selectCard(index, selected) && guessed === 0) setFlip(!useFlip);
+  };
+
   return (
-    <div className="flip-card">
-      <div className="flip-card-inner">
+    <div className={`flip-card`} onClick={handleFlip}>
+      <div className={`flip-card-inner ${useFlip ? "flip" : ""}`}>
         <div className="flip-card-front">
           <p className="title">{frontText}</p>
           <p>Hover Me</p>
--- components/game/findPairGame/Matrix.tsx
+++ components/game/findPairGame/Matrix.tsx
@@ -1,6 +1,13 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Card from "./Card";
 import { DataItem } from "../../../types/types";
+import {
+  checkCardMatch,
+  checkDataSelectable,
+  getSelectedCards,
+  prepareCardDeck,
+} from "../../../utils/utils-data";
+import { CardData } from "../../../types/game";
 
 type MatrixProps = {
   cardData: DataItem[];
@@ -11,20 +18,31 @@ export const Matrix: React.FC<MatrixProps> = ({ cardData }) => {
 
   // Card elements state
   const [useCardElement, setCardElement] = useState<ReactNode>([]);
+  const [useCardDeck, setCardDeck] = useState<CardData[]>([]);
+  const [useRefresh, setRefresh] = useState(false);
+
+  // Initiliaze card deck
+  useEffect(() => {
+    setCardDeck(prepareCardDeck(cardData));
+  }, [cardData]);
 
   // Create all cards
   useEffect(() => {
-    if (cardData.length != 0) {
+    if (useCardDeck.length != 0) {
       const cardList = [];
       let counterKey = 0;
       for (let row = 0; row < GRID_SIZE; row++) {
         for (let col = 0; col < GRID_SIZE; col++) {
-          let actualCard = cardData[counterKey];
+          let actualCard = useCardDeck[counterKey];
           cardList.push(
             <Card
               frontText={actualCard.front}
               backText={actualCard.back}
               key={counterKey}
+              selectCard={handleSelectACard}
+              index={counterKey}
+              guessed={actualCard.guessedFrom}
+              selected={actualCard.selected}
             ></Card>,
           );
           counterKey += 1;
@@ -32,7 +50,68 @@ export const Matrix: React.FC<MatrixProps> = ({ cardData }) => {
       }
       setCardElement(cardList);
     }
-  }, [cardData]);
+  }, [useCardDeck, useRefresh]);
+
+  useEffect(() => {
+    const selectedCards = getSelectedCards(useCardDeck);
+    // Check if the card match
+    if (checkCardMatch(useCardDeck)) {
+      // Change card to gueseed
+      setCardDeck((prevState) => {
+        const newCardDeck = [...prevState];
+        // Change to gueesed
+
+        // Extract index of the selected cards
+        const cardIndex1 = selectedCards[0];
+        const cardIndex2 = selectedCards[1];
+        // Change the state to gueeesed
+        newCardDeck[cardIndex1].guessedFrom = 1;
+        newCardDeck[cardIndex2].guessedFrom = 1;
+        // Change to unselected
+        newCardDeck[cardIndex1].selected = false;
+        newCardDeck[cardIndex2].selected = false;
+        return newCardDeck;
+      });
+      alert("Card gueesed!");
+    } else if (selectedCards.length === 2) {
+      console.log(selectedCards);
+      setCardDeck((prevState) => {
+        const newCardDeck = [...prevState];
+        // Extract index of the selected cards
+        const cardIndex1 = selectedCards[0];
+        const cardIndex2 = selectedCards[1];
+        // Change to unselected
+        newCardDeck[cardIndex1].selected = false;
+        newCardDeck[cardIndex2].selected = false;
+        return newCardDeck;
+      });
+      alert("Wrong!");
+    }
+  }, [useCardDeck]);
+
+  // Handle the selection of a card
+  const handleSelectACard = (index: number, lastState: boolean) => {
+    // Helper function to flip card
+    const flipCard = (prevState: CardData[]) => {
+      const newCardDeck = [...prevState];
+      // Flip the value
+      newCardDeck[index].selected = !lastState;
+      return newCardDeck;
+    };
+
+    // If already flipped filp again
+    if (lastState) {
+      setCardDeck(flipCard);
+      return true;
+    }
+    // Check if there are less than 2 cards selected
+    else if (checkDataSelectable(useCardDeck)) {
+      setCardDeck(flipCard);
+    } else {
+      return false;
+    }
+    return true;
+  };
 
   return (
     <>
