5117f4bcad3d7248369e1bf4eace10a72dcedc27
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-09 17:51:46 +0900

    feat:Add types used in quick answer game

--- types/game.d.ts
+++ types/game.d.ts
@@ -36,3 +36,27 @@ export interface ClientGameState extends ServerGameState {
   username: string
   user_id: number
 }
+
+//quick answer client state in server
+//quick answer game state in client
+
+export type QuickAnswerGameServerState = {
+  players: Player[]
+  problems: contentForQuickAnswer[]
+  deals: contentForQuickAnswer[][]
+  currentTurn: number
+  // rank:
+}
+export interface contentForQuickAnswer {
+  content_id: number,
+  japanese_slang: string,
+  english_slang: string,
+  formal_version: string,
+  description: string
+}
+
+export type QuickAnswerServerLobby = {
+  name: string
+  owner: number // User_id
+  gameState: QuickAnswerGameServerState
+}
