e49509206fd1ff355cf07f42bbcd797eb5814b04
SJSJC <flyflyinu@gmail.com>
2024-09-11 01:06:30 +0900

    Add all start component to each page. URLs work well. Image loader has issue

---
+++ components/game/findPairGame/Start.tsx
@@ -0,0 +1,31 @@
+import Image, { ImageLoader } from 'next/image';
+import gamePic from './../../../public/pair-game-fox.png';
+import Link from 'next/link';
+
+const baseUrl = process.env.NEXTAUTH_URL;
+
+const imageLoader: ImageLoader = ({ src, width, quality }) => {
+  return `${baseUrl}/${src}?w=${width}&q=${quality || 75}`;
+};
+
+const StartPairGame: React.FC = () => {
+  return (
+    <>
+      <Link href={'/game/find-pair'}>
+        <div className="grid grid-cols-2 gap-4 h-full items-center cursor-pointer">
+          <div className="flex justify-center items-center">
+            <div>
+              <h3 className="text-3xl text-center">Find Pair Game</h3>
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <Image loader={imageLoader} width={300} height={300} src={gamePic} alt="Find Pair Fox Picture" />
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default StartPairGame;
--- components/game/quickGame/Start.tsx
+++ components/game/quickGame/Start.tsx
@@ -1,5 +1,6 @@
 import Image, { ImageLoader } from 'next/image';
-import gamePic from './../../../public/practice-nife.png';
+import gamePic from './../../../public/quick-answer-fox.png';
+import Link from 'next/link';
 
 const baseUrl = process.env.NEXTAUTH_URL;
 
@@ -7,21 +8,24 @@ const imageLoader: ImageLoader = ({ src, width, quality }) => {
   return `${baseUrl}/${src}?w=${width}&q=${quality || 75}`;
 };
 
-const PracticeStart: React.FC = () => {
+const StartQuickAnswerGame: React.FC = () => {
   return (
     <>
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-          <span>Practice</span>
-          <br></br>
-          <span> Practice Mode</span>
+      <Link href={'/game/quick-answer'}>
+        <div className="grid grid-cols-2 gap-4 h-full items-center cursor-pointer">
+          <div className="flex justify-center items-center">
+            <div>
+              <h3 className="text-3xl text-center">Quick Answer Game</h3>
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <Image loader={imageLoader} width={300} height={300} src={gamePic} alt="Quick Answer Game Fox Picture" />
+          </div>
         </div>
-        <div>
-          <Image loader={imageLoader} width={300} height={300} src={gamePic} alt="Cross nife picture" />
-        </div>
-      </div>
+      </Link>
     </>
   );
 };
 
-export default PracticeStart;
+export default StartQuickAnswerGame;
