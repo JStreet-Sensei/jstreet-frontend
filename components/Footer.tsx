4eeba1e89f32a9c2f148fa9c4443caf0f1138343
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-12 12:11:46 +0900

    chore:Add Change footer a little

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export const Footer: React.FC = () => {
   return (
     <>
-      <div>This is footer! Our repo is here!</div>
+      <div className="m-10">
+        <div>Thanks for start playing!</div>
+        <Link className="" href={"https://github.com/Nihongo-Jouzu/nihongo-jouzu-frontend"}>Our repo Link for frontend</Link><br></br>
+        <Link href={"https://github.com/Nihongo-Jouzu/nihongo-jouzu-backend"}>Our repo Link for backend</Link>
+      </div>
     </>
   );
 };
