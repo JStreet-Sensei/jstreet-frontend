23638e3508e28d3909beec076cb909bfcb7de596
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-12 14:28:43 +0900

    chore:Just comment out some part

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,30 +36,34 @@ const HomePage = () => {
           Go to Lobby
         </Link>
       </div>
-      <div className={styles.graffiti}>
+
+      {/* <div className={styles.graffiti}>
         <div className="relative flex flex-col items-center justify-center min-h-screen bg-[url('/LandingPage/wave_top_draw.svg')] bg-no-repeat bg-top bg-contain">
           <div className="absolute inset-0 bg-[url('/LandingPage/wave_bottom_draw.svg')] bg-no-repeat bg-bottom bg-contain z-0" />
           <div className="flex-1 max-w-xl flex items-center justify-center flex-col z-10 ml-96">
             <p className="ml-96">You are not authenticated.</p>
-            <button
-              onClick={() => {
-                signIn(undefined, { callbackUrl: '/mypage' });
-              }}
-              className="m-10 bg-red-700 text-white font-bold py-4 px-8 rounded-full shadow-lg hover:bg-red-600  focus:ring-2 focus:ring-opacity-75 transition duration-300 ease-in-out ml-96"
-            >
-              Sign In
-            </button>
-
-            <button className="m-10 bg-red-700 text-white font-bold py-4 px-8 rounded-full shadow-lg hover:bg-red-600  focus:ring-2 focus:ring-opacity-75 transition duration-300 ease-in-out ml-96">
-              <Link href={"/signup"}>
-                Sign Up
-              </Link>
-            </button>
           </div>
         </div>
         <Image src="/LandingPage/oni.svg" alt="Oni" width={400} height={100} className="absolute top-0 left-0 " />
         <Image src="/LandingPage/wild_talk.png" alt="Wild Talk" width={600} height={100} className="mx-auto my-4" />
         <Image src="/LandingPage/street1.jpg" alt="Wild Talk" width={800} height={100} className="mx-auto my-4" />
+      </div> */}
+      <div>
+
+        <button
+          onClick={() => {
+            signIn(undefined, { callbackUrl: '/mypage' });
+          }}
+          className="m-10 bg-red-700 text-white font-bold py-4 px-8 rounded-full shadow-lg hover:bg-red-600  focus:ring-2 focus:ring-opacity-75 transition duration-300 ease-in-out ml-96"
+        >
+          Sign In
+        </button>
+
+        <button className="m-10 bg-red-700 text-white font-bold py-4 px-8 rounded-full shadow-lg hover:bg-red-600  focus:ring-2 focus:ring-opacity-75 transition duration-300 ease-in-out ml-96">
+          <Link href={"/signup"}>
+            Sign Up
+          </Link>
+        </button>
       </div>
     </>
   );
