b8ba32e9a22a3b85af723c87927e4f6f74796c8a
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-12 11:57:40 +0900

    style:Add minimal css

--- pages/auth/credentials-signin.tsx
+++ pages/auth/credentials-signin.tsx
@@ -8,17 +8,25 @@ export default function SignIn({
     csrfToken,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
-        <form method="post" action="/api/auth/callback/credentials">
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <label>
-                Username
-                <input name="username" type="text" />
-            </label>
-            <label>
-                Password
-                <input name="password" type="password" />
-            </label>
-            <button type="submit">Sign in</button>
+        <form method="post" action="/api/auth/callback/credentials" className="m-10 flex flex-col items-center">
+            <div className="m-10">
+                <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+                <label>
+                    Username
+                    <input name="username" type="text" className="ml-5 border-black border-2 p-2 rounded-lg" />
+                </label>
+            </div>
+            <div className="m-10">
+                <label>
+                    Password
+                    <input name="password" type="password" className="ml-5 border-black border-2 p-2 rounded-lg" />
+                </label>
+            </div>
+            <div>
+                <button type="submit" className="bg-[var(--turquoise)] text-[var(--blue-dark)] font-semibold 
+            py-3 px-10 rounded-lg shadow-md hover:bg-[var(--magenta)] 
+            hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 m-4 w-40">Sign in</button>
+            </div>
         </form>
     )
 }
