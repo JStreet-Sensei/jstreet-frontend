ffa907c1c25e4fbe473b0fe8d36846fe22d52aa1
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-13 14:44:15 +0900

    style:Add responsive one

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Footer: React.FC = () => {
   return (
     <>
-      <div className="fixed bottom-0 flex flex-row justify-around items-center w-full bg-[#f179a7] h-20 font-bold">
+      <div className="z-10 portrait:hidden landscape:visible fixed bottom-0 flex flex-row justify-around items-center w-full bg-[#f179a7] h-20 font-bold">
         <div className="flex justify-center items-center w-1/3">Thanks for start playing!</div>
         <Link className="flex justify-center items-center w-1/3 underline cursor-pointer" href={"https://github.com/Nihongo-Jouzu/nihongo-jouzu-frontend"}>Our repo Link for frontend</Link>
         <Link className="flex justify-center items-center w-1/3 underline cursor-pointer" href={"https://github.com/Nihongo-Jouzu/nihongo-jouzu-backend"}>Our repo Link for backend</Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,13 @@ const HomePage = () => {
   return (
     <>
       <div
-        className="relative flex flex-col items-start justify-center min-h-screen bg-[url('/LandingPage/landpage.svg')] bg-no-repeat bg-cover bg-center"
+        className="relative flex flex-col items-start justify-center min-h-screen
+        bg-no-repeat bg-center md:hidden bg-[#25dad1] " style={{ backgroundImage: "url('/LandingPage/landpage_md.jpeg')", backgroundSize: "80%" }}
       >
-        <div className="flex-1 max-w-xl flex items-start justify-center flex-col z-10 px-4"> {/* Adiciona padding para afastar do canto */}
-          <div className="fixed z-20 flex flex-row gap-4 bottom-72 left-4 items-center justify-center">
+
+        <div className="flex-1 min-w-full flex items-start justify-center flex-col z-10"> {/* Adiciona padding para afastar do canto */}
+
+          <div className="fixed z-20 flex flex-row gap-4 bottom-72 left-4 items-center justify-center ">
             <button
               onClick={() => {
                 signIn(undefined, { callbackUrl: '/mypage' });
@@ -33,6 +36,27 @@ const HomePage = () => {
               <Link href={"/signup"}>Sign Up</Link>
             </button>
           </div>
+        </div>
+      </div>
+
+      <div className='relative md:flex flex-col items-start justify-center min-h-screen
+        bg-no-repeat bg-[#25dbd1] bg-center hidden md:visible bg-contain' style={{ backgroundImage: "url('/LandingPage/landpage.svg')", backgroundSize: "100%" }}>
+        <div className="fixed z-20 flex flex-row gap-4 bottom-72 left-40 items-center justify-center ">
+          <button
+            onClick={() => {
+              signIn(undefined, { callbackUrl: '/mypage' });
+            }}
+            className="bg-red-700 h-10 text-white font-bold py-4 px-8 rounded-full left-2
+              bottom-1 shadow-lg hover:bg-red-600 focus:ring-2 focus:ring-opacity-75 transition duration-300 ease-in-out"
+          >
+            Sign In
+          </button>
+          <span className=" h-10  text-white font-bold py-4 px-8 text-2xl
+              ">OR</span>
+          <button className="bg-red-700 h-10 rounded-full  left-80
+              bottom-1  text-white font-bold py-4 px-8  shadow-lg hover:bg-red-600 focus:ring-2 focus:ring-opacity-75 transition duration-300 ease-in-out">
+            <Link href={"/signup"}>Sign Up</Link>
+          </button>
         </div>
       </div>
 
