49162ee271f68bb319309fb402feca67f293b788
Eiichi Sayama <eiichimouse@outlook.jp>
2024-09-15 00:08:07 +0900

    feat:Add Seo component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { Layout } from '../components/Layouts';
 import { SessionProvider } from 'next-auth/react';
+import Seo from '@/components/Seo';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Seo></Seo>
       <Layout>
         <Component {...pageProps} />
       </Layout>
