601635b9ab6e5d20f0b43befdb26ff702f976fbc
SJSJC <flyflyinu@gmail.com>
2024-09-14 15:17:58 +0900

    Add lobby icon

--- pages/lobby.tsx
+++ pages/lobby.tsx
@@ -88,7 +88,41 @@ const Lobby: React.FC = () => {
   return (
     <>
       <div className="relative p-6 w-webkit-fill-available">
-        <h2 className="text-2xl font-bold mb-4 ">Lobbies</h2>
+        <h2 className="text-2xl font-bold mb-4 flex items-center">
+          Lobbies
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 47.41" className="h-7 ml-2">
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="layer_1-2" data-name="layer 1">
+                <path
+                  className="cls-1"
+                  d="M20 43.71H7a7 7 0 0 1-7-7v-26a7 7 0 0 1 7-7h17v2H7a5 5 0 0 0-5 5v26a5 5 0 0 0 5 5h13zM41 43.71H24v-2h17a5 5 0 0 0 5-5v-26a5 5 0 0 0-5-5H28v-2h13a7 7 0 0 1 7 7v26a7 7 0 0 1-7 7z"
+                />
+                <path
+                  className="cls-1"
+                  d="M20.71 9.41 19.29 8l3.3-3.29-3.3-3.3L20.71 0l4 4a1 1 0 0 1 0 1.41zM27.29 47.41l-4-4a1 1 0 0 1 0-1.41l4-4 1.42 1.41-3.3 3.3 3.3 3.29zM17 20.71c-2.76 0-5-2.69-5-6a4.68 4.68 0 0 1 5-5 4.68 4.68 0 0 1 5 5c0 3.29-2.24 6-5 6zm0-9a2.72 2.72 0 0 0-3 3c0 2.2 1.35 4 3 4s3-1.8 3-4a2.72 2.72 0 0 0-3-3zM11 33.71h2v6h-2zM16 36.71h2v3h-2zM21 33.71h2v6h-2z"
+                />
+                <path
+                  className="cls-1"
+                  d="M12 36.71h-2a3 3 0 0 1-3-3v-9.42a5 5 0 0 1 4.45-5l5.44-.61.22 2-5.44.61a3 3 0 0 0-2.67 3v9.42a1 1 0 0 0 1 1h1v-10h2v11a1 1 0 0 1-1 1z"
+                />
+                <path
+                  className="cls-1"
+                  d="M24 36.71h-2a1 1 0 0 1-1-1v-11h2v10h1a1 1 0 0 0 1-1v-9.42a3 3 0 0 0-2.67-3l-5.44-.61.22-2 5.44.61a5 5 0 0 1 4.45 5v9.42a3 3 0 0 1-3 3z"
+                />
+                <path
+                  className="cls-1"
+                  d="M12 32.71h10v2H12zM31 20.71c-2.76 0-5-2.69-5-6a4.68 4.68 0 0 1 5-5 4.68 4.68 0 0 1 5 5c0 3.29-2.24 6-5 6zm0-9a2.72 2.72 0 0 0-3 3c0 2.2 1.35 4 3 4s3-1.8 3-4a2.72 2.72 0 0 0-3-3zM30 36.71h2v3h-2zM35 33.71h2v6h-2z"
+                />
+                <path
+                  className="cls-1"
+                  d="M38 36.71h-2a1 1 0 0 1-1-1v-11h2v10h1a1 1 0 0 0 1-1v-9.42a3 3 0 0 0-2.67-3l-5.44-.61.22-2 5.44.61a5 5 0 0 1 4.45 5v9.42a3 3 0 0 1-3 3z"
+                />
+                <path className="cls-1" d="M28 32.71h8v2h-8z" />
+              </g>
+            </g>
+          </svg>
+        </h2>
+
         <p className="mb-10">
           Select an existing Lobby from the list below, or click &quot;Create Lobby&quot; to Open a Lobby.
         </p>
