b823c7e59d9868c7b5967e66beb3fd5f56fabf5f
SJSJC <flyflyinu@gmail.com>
2024-09-16 18:54:40 +0900

    Update layout of the page. Update catch phrases.

--- components/game/flashCard/Start.tsx
+++ components/game/flashCard/Start.tsx
@@ -4,20 +4,22 @@ import Link from 'next/link';
 const StartFlashCard: React.FC = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row h-full items-center ">
-        <div className='flex flex-col'>
+      <div className="flex md:flex-row flex-col h-full items-center">
+        <div className="flex flex-col">
           <h3 className="text-3xl text-start">Practice Mode</h3>
-          <h5>Words already learned appears.<br></br>Try to remember what you memorized.</h5>
+          <h5>
+            Check learned words in Expressions.<br></br>Memorize with Flashcards!
+          </h5>
         </div>
         <Image width={150} height={150} src="/practice-nife.png" alt="Cross nife picture" />
       </div>
-      <div className='ml-20'>
+      <div className="ml-20">
         <Link href={'/game/flash-card'}>
           <button
             className="bg-[var(--savoy-blue)] text-white py-2 
           rounded-full transform -translate-y-1/2 top-1/2 hover:bg-[var(--tekhelet)] transition
-          px-8
-          cursor-pointer"
+          px-8 cursor-pointer 
+          "
           >
             Start!
           </button>
--- components/game/newWords/Start.tsx
+++ components/game/newWords/Start.tsx
@@ -5,19 +5,22 @@ const LearningStart: React.FC = () => {
   return (
     <>
       <div className="flex md:flex-row flex-col h-full items-center">
-        <div className='flex flex-col'>
-          <h3 className="text-3xl text-start">Expression</h3><br></br>
-          <h5>You can read many word cards. <br></br>Remenber and test in Practice mode.</h5>
+        <div className="flex flex-col">
+          <h3 className="text-3xl text-start">Expression</h3>
+          <br></br>
+          <h5>
+            Learn Casual Street Japanese! <br></br>Translations and context are ready <br></br>for you.
+          </h5>
         </div>
         <Image width={150} height={150} src="/new-expression-fox.png" alt="Fox picture" />
       </div>
-      <div className='ml-20'>
+      <div className="ml-20">
         <Link href={'/game/expression'}>
           <button
-            className="mt-5 bg-[var(--savoy-blue)] text-white py-2 
+            className="bg-[var(--savoy-blue)] text-white py-2 
           rounded-full transform -translate-y-1/2 top-1/2 hover:bg-[var(--tekhelet)] transition
-          px-8
-          cursor-pointer "
+          px-8 cursor-pointer 
+          "
           >
             Start!
           </button>
