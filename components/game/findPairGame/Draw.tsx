a086105bc708c11347b5cf5c4cccfb8c862d3f1f
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-09-19 12:41:43 +0900

    Style: Draw result

--- components/game/findPairGame/Draw.tsx
+++ components/game/findPairGame/Draw.tsx
@@ -2,7 +2,9 @@ export const Draw = () => {
   return (
     <>
       <div className="mx-auto flex items-center justify-center">
-        <p className="text-4xl font-extrabold leading-tight secondary-inverse mt-10 px-3">Draw! Ready for rematch?</p>
+        <p className="text-4xl font-extrabold leading-tight secondary-inverse mt-10 px-3 border-2 border-white p-2 rounded-md m-2 bg-white shadow-lg">
+          Draw! Ready for rematch?
+        </p>
       </div>
     </>
   );
